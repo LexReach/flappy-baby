77ee1356062edd480b729c95066393f51097ee05
LexReach <alexpennington859@gmail.com>
2025-12-01 00:33:00 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("flappy-baby-cache").then(cache => {
-      return cache.addAll(["./", "index.html", "style.css", "game.js", "assets/baby.png"]);
-    })
+    caches.open("flappy-baby-cache").then(cache =>
+      cache.addAll([
+        "./",
+        "index.html",
+        "style.css",
+        "game.js",
+        "assets/baby.png",
+        "assets/millie.png",
+        "assets/gameover.png"
+      ])
+    )
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
